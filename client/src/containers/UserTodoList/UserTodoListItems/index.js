d68e6d4cda780816865ceddb4f4bbd8bbf03d793
Paul Han <paul.myung.han@gmail.com>
2020-05-20 19:46:06 -0700

    created complete button

--- client/src/containers/UserTodoList/UserTodoListItems/index.js
+++ client/src/containers/UserTodoList/UserTodoListItems/index.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { Header, List, Button } from "semantic-ui-react";
+import React from 'react';
+import { Header, List, Button } from 'semantic-ui-react';
+
 
 export default (props) => {
-    if (props.todos.length === 0){
-        return <Header content="No todos yet, please add a todo"/>
-    } else {
-        return props.todos.map(({_id, completed, text}) => {
-            return(
-                <List.Item key={_id}>
-                    <List.Content floated="left">
-                        <p style={{ textDecoration: completed ? "line-through" : "none", fontSize="20px"}}>{text}</p>
-                    </List.Content>
-                    <List.Content floated="right">
-
-                    </List.Content>
-                </List.Item>
-            )
-        });
-    }
-}
+  if (props.todos.length === 0) {
+    return <Header content='No todos yet, please add a todo'/>
+  } else {
+    return props.todos.map(({_id, completed, text}) => {
+      return (
+        <List.Item key={_id}>
+          <List.Content floated='left'>
+            <p style={{ textDecoration: completed ? 'line-through' : 'none', fontSize: '20px'}} >{text}</p>
+          </List.Content>
+          <List.Content floated='right'>
+            <Button color='blue' content='Mark Complete' size='small'/>
+          </List.Content>
+        </List.Item>
+      );
+    });
+  }
+};
