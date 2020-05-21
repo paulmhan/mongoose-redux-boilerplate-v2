59a006620899d40145f1310f0473522fdfca7f92
Paul Han <paul.myung.han@gmail.com>
2020-05-21 13:43:27 -0700

    types correction

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,4 @@
-//counting section
+//counter section
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
@@ -6,16 +6,19 @@ export const DECREMENT = 'DECREMENT';
 export const GET_ALL_TODOS = 'GET_ALL_TODOS';
 export const GET_ALL_TODOS_ERROR = 'GET_ALL_TODOS_ERROR';
 
-//user auth
-export const AUTH_USER = "AUTH_USER";
-export const AUTH_USER_ERROR = "AUTH_USER_ERROR";
 
-//user todos section
-export const GET_USER_TODOS = "GET_USER_TODOS";
-export const GET_USER_TODOS_ERROR = "GET_USER_TODOS_ERROR";
+// User Todos Section
+export const GET_USER_TODOS = 'GET_USER_TODOS';
+export const GET_USER_TODOS_ERROR = 'GET_USER_TODOS_ERROR';
 
-//add user todos section
-export const ADD_TODO = "ADD_TODO";
-export const ADD_TODO_ERROR = "ADD_TODO_ERROR";
+// Add user todos section
+export const ADD_TODO = 'ADD_TODO';
+export const ADD_TODO_ERROR = 'ADD_TODO_ERROR';
 
 
+// Update User Todo by ID
+export const UPDATE_TODO_BY_ID_ERROR = 'UPDATE_TODO_BY_ID_ERROR';
+
+//  User authentication
+export const AUTH_USER = 'AUTH_USER';
+export const AUTH_USER_ERROR = 'AUTH_USER_ERROR';
